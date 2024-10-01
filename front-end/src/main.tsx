fc08fc3c00807f93ba516dce457bc059f05f65cc
Vu Hoang Tien <vht.20021020@gmail.com>
2024-10-01 09:28:34 +0700

    feat(fe/feature/update_theme): update theme

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react';
+import App from '@/App';
+import theme from '@/theme';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <CssVarsProvider theme= {theme}>
+      <CssBaseline/>
+      <App />
+  </CssVarsProvider>
 );
