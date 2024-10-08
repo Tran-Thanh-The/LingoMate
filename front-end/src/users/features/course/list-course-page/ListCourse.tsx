619249580041d8cad383219ebc85c8aadc0b4451
Vu Hoang Tien <vht.20021020@gmail.com>
2024-10-08 10:54:58 +0700

    feat(fe/feature/course): implement excellent student card

--- front-end/src/users/features/course/list-course-page/ListCourse.tsx
+++ front-end/src/users/features/course/list-course-page/ListCourse.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './components/banner/Banner';
 import ListCourses from './components/list-courses/ListCourses';
+import ExcellentMember from './components/excellent-member/ExcellentMember';
 
 const ListCourse = () => {
   return (
     <>
       <Banner />
       <ListCourses />
+      <ExcellentMember />
     </>
   );
 };
