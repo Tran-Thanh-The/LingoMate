09a38610fb678182734cc562870f1df21bb03579
Vu Hoang Tien <vht.20021020@gmail.com>
2024-10-10 10:14:36 +0700

    fix(be/feature/mailer): fix status register user

--- lingomate-be/src/auth/auth.service.ts
+++ lingomate-be/src/auth/auth.service.ts
@@ -212,7 +212,7 @@ export class AuthService {
           id: RoleEnum.user,
         },
         status: {
-          id: StatusEnum.active,
+          id: StatusEnum.inactive,
         },
       });
 
